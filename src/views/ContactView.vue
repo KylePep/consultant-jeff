cdee302f8e663194438cc9bf1f19f59fe54f43bb
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-03-09 17:00:09 -0600

    padding on contact buttons

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -50,13 +50,13 @@ import SectionLayout from "@/layouts/SectionLayout.vue";
 
           <ul class="space-y-8">
             <a href="tel:2089998966" class="flex items-center bg-fbGreen py-1 text-white rounded">
-              <i class="mdi mdi-phone text-2xl px-8"></i>
+              <i class="mdi mdi-phone text-2xl px-4 lg:px-8"></i>
               208-999-8966
             </a>
 
             <a href="mailto:jeff.mcbride@heronconsulting.us"
               class="flex items-center bg-fbGreen py-1 text-white rounded">
-              <i class="mdi mdi-email text-2xl px-8"></i>
+              <i class="mdi mdi-email text-2xl px-4 lg:px-8"></i>
               jeff.mcbride@heronconsulting.us
             </a>
             <li class="font-semibold text-sm lg:text-xl">
