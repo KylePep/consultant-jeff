fac11db4a2e61efa83b0ab442bde8798c5a4588b
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2025-03-09 13:34:15 -0600

    fixed custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        page: "rgb(var(--color-fb-white))",
-        fb: {
-          DEFAULT: "rgb(var(--color-fb-light))",
-          light: "rgb(var(--color-fb-light))",
-          gray: "rgb(var(--color-fb-gray))",
-          green: "rgb(var(--color-fb-green))",
-          white: "rgb(var(--color-fb-white))",
-        },
       },
       fontFamily: {
         sans: ['Funnel Sans', ...defaultTheme.fontFamily.sans],
@@ -28,4 +20,3 @@ export default {
   ],
 
 }
-
